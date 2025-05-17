5c83e7b78a76bd00d7e96e37bb1efb5694842636
Nikola Hadzic <bibilionyt@gmail.com>
2025-05-17 23:06:18 +0200

    project setup

---
+++ PasswordGenerator/InfoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace PasswordGeneratorPro
+{
+    public partial class InfoWindow : Window
+    {
+        public InfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void GitHubButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/hadzicni/Password-Generator",
+                UseShellExecute = true
+            });
+        }
+    }
+}
---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+
+namespace PasswordGeneratorPro
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Random random = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LengthSlider.ValueChanged += (s, e) => LengthLabel.Text = $"Länge: {LengthSlider.Value}";
+            SelectAllCheckbox.Checked += (s, e) => SetAllCheckboxes(true);
+            SelectAllCheckbox.Unchecked += (s, e) => SetAllCheckboxes(false);
+            Loaded += (_, _) => GeneratePassword(); // Beim Start direkt ein Passwort generieren
+        }
+
+        private void SetAllCheckboxes(bool value)
+        {
+            UseUpper.IsChecked = value;
+            UseLower.IsChecked = value;
+            UseDigits.IsChecked = value;
+            UseSymbols.IsChecked = value;
+        }
+
+        private void GeneratePassword_Click(object sender, RoutedEventArgs e)
+        {
+            GeneratePassword();
+        }
+
+        private void GeneratePassword()
+        {
+            int length = (int)LengthSlider.Value;
+
+            var charPool = new StringBuilder();
+            if (UseUpper.IsChecked == true) charPool.Append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            if (UseLower.IsChecked == true) charPool.Append("abcdefghijklmnopqrstuvwxyz");
+            if (UseDigits.IsChecked == true) charPool.Append("0123456789");
+            if (UseSymbols.IsChecked == true) charPool.Append("!@#$%^&*()_-+=<>?");
+
+            if (charPool.Length == 0)
+            {
+                MessageBox.Show("Bitte mindestens eine Zeichengruppe auswählen.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var password = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(charPool.Length);
+                password.Append(charPool[index]);
+            }
+
+            PasswordBox.Text = password.ToString();
+            EvaluateStrength(password.ToString());
+        }
+
+        private void CopyToClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PasswordBox.Text))
+            {
+                Clipboard.SetText(PasswordBox.Text);
+                MessageBox.Show("Passwort wurde kopiert.", "Erfolg", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void EvaluateStrength(string password)
+        {
+            int score = 0;
+
+            if (password.Length >= 8) score++;
+            if (password.Length >= 12) score++;
+            if (Regex.IsMatch(password, "[A-Z]")) score++;
+            if (Regex.IsMatch(password, "[a-z]")) score++;
+            if (Regex.IsMatch(password, "[0-9]")) score++;
+            if (Regex.IsMatch(password, "[^a-zA-Z0-9]")) score++;
+
+            score = Math.Min(score, 5);
+            StrengthBar.Value = score;
+
+            string level;
+            if (score <= 2) level = "Schwach";
+            else if (score == 3) level = "Mittel";
+            else level = "Stark";
+
+            StrengthLabel.Text = $"Sicherheitsbewertung: {level}";
+            StrengthBar.Foreground = level switch
+            {
+                "Schwach" => new SolidColorBrush(Colors.OrangeRed),
+                "Mittel" => new SolidColorBrush(Colors.Goldenrod),
+                "Stark" => new SolidColorBrush(Colors.LimeGreen),
+                _ => new SolidColorBrush(Colors.Gray)
+            };
+        }
+
+        private void OpenInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var info = new InfoWindow
+            {
+                Owner = this
+            };
+            info.ShowDialog();
+        }
+
+    }
+}
